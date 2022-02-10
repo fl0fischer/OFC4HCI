ce4c4948c795c130d58133d271588e23ebf304fd
fl0fischer <62909493+fl0fischer@users.noreply.github.com>
2022-02-10 11:27:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='ofc4hci',
       author_email='florian.j.fischer@uni-bayreuth.de',
       url='https://github.com/fl0fischer/OFC4HCI',
       license='LICENSE',
-      description='This python script includes all OFC methods described in the ToCHI paper "Optimal Feedback Control for Modeling Human-Computer Interaction" (2OL-Eq, MinJerk, LQR, LQG, and E-LQG).',
+      description='This python script includes all OFC methods described in the paper "Optimal Feedback Control for Modeling Human-Computer Interaction" (2OL-Eq, MinJerk, LQR, LQG, and E-LQG).',
       long_description=open('README.md').read(),
       python_requires='>= 3.8',
       install_requires=['numpy', 'scipy', 'pandas', 'matplotlib']
