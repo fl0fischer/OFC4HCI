9157496fc8314e6bbe84ce1b511f655d96fdd085
fl0fischer <62909493+fl0fischer@users.noreply.github.com>
2022-02-10 11:21:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='ofc4hci',
-      version='0.0.1',
+      version='0.1.0',
       author='Florian Fischer',
       author_email='florian.j.fischer@uni-bayreuth.de',
       url='https://github.com/fl0fischer/OFC4HCI',
